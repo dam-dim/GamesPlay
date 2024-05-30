3d0015b017017e52ef8c4a3d7d65b222f06cba88
Damyan Dimov <dam.dimov99@gmail.com>
2024-05-30 12:12:28 +0300

    extracted html into components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,14 @@
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Main from "./components/Main";
+
 function App() {
-    return <></>;
+    return (
+        <>
+            <Header />
+            <Home />
+        </>
+    );
 }
 
 export default App;
