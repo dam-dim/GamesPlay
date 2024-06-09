c1679773cc451125d7d7791b3d168b112f2077b4
Damyan Dimov <dam.dimov99@gmail.com>
2024-06-09 14:58:29 +0300

    Fixed folder structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
-import Header from "./components/Header";
-import Home from "./components/Home";
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
 
 function App() {
     return (
-        <>
+        <div id="box">
             <Header />
             <Home />
-        </>
+        </div>
     );
 }
 
